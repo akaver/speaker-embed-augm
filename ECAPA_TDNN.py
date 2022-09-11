c684e08f0d24e2842bfd5c2ec5a28047a918b89e
akaver <akaver@akaver.com>
2022-09-11 22:19:05 +0300

    ecapa_tdnn eer mindcf

--- ECAPA_TDNN.py
+++ ECAPA_TDNN.py
@@ -11,10 +11,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from packaging import version
-from typing import TypeVar, Generic, Iterable, Iterator, Sequence, List, Optional, Tuple
-import bisect
-import random
-import warnings
 
 logger = logging.getLogger(__name__)
 
@@ -2404,3 +2400,123 @@ def compute_masked_loss(
             loss_reg = loss_reg.sum(1) / mask.sum(1)
 
         return -label_smoothing * loss_reg + (1 - label_smoothing) * loss
+
+
+def EER(positive_scores, negative_scores):
+    """Computes the EER (and its threshold).
+
+    Arguments
+    ---------
+    positive_scores : torch.tensor
+        The scores from entries of the same class.
+    negative_scores : torch.tensor
+        The scores from entries of different classes.
+
+    Example
+    -------
+    >>> positive_scores = torch.tensor([0.6, 0.7, 0.8, 0.5])
+    >>> negative_scores = torch.tensor([0.4, 0.3, 0.2, 0.1])
+    >>> val_eer, threshold = EER(positive_scores, negative_scores)
+    >>> val_eer
+    0.0
+    """
+
+    # Computing candidate thresholds
+    thresholds, _ = torch.sort(torch.cat([positive_scores, negative_scores]))
+    thresholds = torch.unique(thresholds)
+
+    # Adding intermediate thresholds
+    interm_thresholds = (thresholds[0:-1] + thresholds[1:]) / 2
+    thresholds, _ = torch.sort(torch.cat([thresholds, interm_thresholds]))
+
+    # Variable to store the min FRR, min FAR and their corresponding index
+    min_index = 0
+    final_FRR = 0
+    final_FAR = 0
+
+    for i, cur_thresh in enumerate(thresholds):
+        pos_scores_threshold = positive_scores <= cur_thresh
+        FRR = (pos_scores_threshold.sum(0)).float() / positive_scores.shape[0]
+        del pos_scores_threshold
+
+        neg_scores_threshold = negative_scores > cur_thresh
+        FAR = (neg_scores_threshold.sum(0)).float() / negative_scores.shape[0]
+        del neg_scores_threshold
+
+        # Finding the threshold for EER
+        if (FAR - FRR).abs().item() < abs(final_FAR - final_FRR) or i == 0:
+            min_index = i
+            final_FRR = FRR.item()
+            final_FAR = FAR.item()
+
+    # It is possible that eer != fpr != fnr. We return (FAR  + FRR) / 2 as EER.
+    EER = (final_FAR + final_FRR) / 2
+
+    return float(EER), float(thresholds[min_index])
+
+
+def minDCF(
+    positive_scores, negative_scores, c_miss=1.0, c_fa=1.0, p_target=0.01
+):
+    """Computes the minDCF metric normally used to evaluate speaker verification
+    systems. The min_DCF is the minimum of the following C_det function computed
+    within the defined threshold range:
+
+    C_det =  c_miss * p_miss * p_target + c_fa * p_fa * (1 -p_target)
+
+    where p_miss is the missing probability and p_fa is the probability of having
+    a false alarm.
+
+    Arguments
+    ---------
+    positive_scores : torch.tensor
+        The scores from entries of the same class.
+    negative_scores : torch.tensor
+        The scores from entries of different classes.
+    c_miss : float
+         Cost assigned to a missing error (default 1.0).
+    c_fa : float
+        Cost assigned to a false alarm (default 1.0).
+    p_target: float
+        Prior probability of having a target (default 0.01).
+
+
+    Example
+    -------
+    >>> positive_scores = torch.tensor([0.6, 0.7, 0.8, 0.5])
+    >>> negative_scores = torch.tensor([0.4, 0.3, 0.2, 0.1])
+    >>> val_minDCF, threshold = minDCF(positive_scores, negative_scores)
+    >>> val_minDCF
+    0.0
+    """
+
+    # Computing candidate thresholds
+    thresholds, _ = torch.sort(torch.cat([positive_scores, negative_scores]))
+    thresholds = torch.unique(thresholds)
+
+    # Adding intermediate thresholds
+    interm_thresholds = (thresholds[0:-1] + thresholds[1:]) / 2
+    thresholds, _ = torch.sort(torch.cat([thresholds, interm_thresholds]))
+
+    # Computing False Rejection Rate (miss detection)
+    positive_scores = torch.cat(
+        len(thresholds) * [positive_scores.unsqueeze(0)]
+    )
+    pos_scores_threshold = positive_scores.transpose(0, 1) <= thresholds
+    p_miss = (pos_scores_threshold.sum(0)).float() / positive_scores.shape[1]
+    del positive_scores
+    del pos_scores_threshold
+
+    # Computing False Acceptance Rate (false alarm)
+    negative_scores = torch.cat(
+        len(thresholds) * [negative_scores.unsqueeze(0)]
+    )
+    neg_scores_threshold = negative_scores.transpose(0, 1) > thresholds
+    p_fa = (neg_scores_threshold.sum(0)).float() / negative_scores.shape[1]
+    del negative_scores
+    del neg_scores_threshold
+
+    c_det = c_miss * p_miss * p_target + c_fa * p_fa * (1 - p_target)
+    c_min, min_index = torch.min(c_det, dim=0)
+
+    return float(c_min), float(thresholds[min_index])
